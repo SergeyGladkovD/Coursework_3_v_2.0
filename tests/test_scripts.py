23402315e5f0cf11b38a94b59034d29d9b8fb614
Gladkov Sergey <melcor2006@mail.ru>
2024-01-28 09:58:23 +0300

    commit  all

--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -1,3 +1,9 @@
+import pytest
+
+
+from scripts.scripts import time_correct, five_items
+
+
 def test_get_transactions():
 	pass
 
@@ -7,11 +13,11 @@ def test_sort_transactions():
 
 
 def test_five_items():
-	pass
+	assert five_items([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) == [1, 2, 3, 4, 5]
 
 
 def test_time_correct():
-	pass
+	assert time_correct("2019-08-26T10:50:58.294041") == '26-08-2019'
 
 
 def test_get_from():
